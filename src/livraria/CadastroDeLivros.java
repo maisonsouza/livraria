0cfc374b098800f6a2bfe1cda908dbed793a9698
Maison <Maison@Maison-PC>
2016-05-12 23:25:51 -0400

    Oganizando o código

--- src/livraria/CadastroDeLivros.java
+++ src/livraria/CadastroDeLivros.java
@@ -3,36 +3,44 @@ package livraria;
 public class CadastroDeLivros {
 
 	public static void main(String[] args) {
+		
+		Autor autor = new Autor();
+		autor.setNome("Mauricio Aniche");
+		autor.setCpf("845244525854");
+		autor.setEmail("mauricio.aniche@caelum.com.br");
+		
+		
+		
 		Livro livro = new Livro();
 		livro.setNome("Java 8 Prático");
 		livro.setDescricao("Novos recursos da linguagem");
 		livro.setValor(59.90);
 		livro.setIsbn("978-2855-2547");
+		livro.setAutor(autor);
+		livro.mostrarDetalhes();
 		
 		
 		livro.adicionaLinha();
+		
+		Autor outroautor = new Autor();
+		outroautor.setNome("Maison Marcel");
+		outroautor.setCpf("98225723289");
+		outroautor.setEmail("rodrigo.silveira@caelum.com.br");
+		
 		
 		Livro outrolivro = new Livro();
 		outrolivro.setNome("Lógica de Programação");
 		outrolivro.setDescricao("Crie seus primeiros programas");
 		outrolivro.setValor(25.68);
 		outrolivro.setIsbn("154984-4554-44654");
-		
-		
-		Autor autor = new Autor();
-		autor.setNome("Mauricio Aniche");
-		autor.setCpf("845244525854");
-		autor.setEmail("mauricio.aniche@caelum.com.br");
-		livro.setAutor(autor);
-		livro.mostrarDetalhes();
-		
-		livro.adicionaLinha();
-		Autor outroautor = new Autor();
-		outroautor.setNome("Maison Marcel");
-		outroautor.setCpf("98225723289");
-		outroautor.setEmail("rodrigo.silveira@caelum.com.br");
 		outrolivro.setAutor(outroautor);
 		outrolivro.mostrarDetalhes();
+		
+		
+		
+		
+		livro.adicionaLinha();
+		
 		
 		livro.aplicaDesdeconto(0.1);
 		livro.aplicaDesdeconto(0.3);
